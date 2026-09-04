02892dfce972c3d81a9973f32845018f1ba32b64
551UK <wdf@hotmail.co.uk>
2026-09-04 06:53:22 +0100

    Add settings header icon and repo button action

--- PillVolumePrefs/PVRootListController.m
+++ PillVolumePrefs/PVRootListController.m
@@ -4,11 +4,53 @@
 
 @implementation PVRootListController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UIImage *image = [UIImage systemImageNamed:@"speaker.wave.2.fill"];
+    UIImageView *iconView = [[UIImageView alloc] initWithImage:image];
+    iconView.tintColor = UIColor.labelColor;
+    iconView.contentMode = UIViewContentModeScaleAspectFit;
+    iconView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    UILabel *titleLabel = [UILabel new];
+    titleLabel.text = @"PillVolume";
+    titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    titleLabel.textColor = UIColor.labelColor;
+
+    UIStackView *titleView = [[UIStackView alloc] initWithArrangedSubviews:@[iconView, titleLabel]];
+    titleView.axis = UILayoutConstraintAxisHorizontal;
+    titleView.alignment = UIStackViewAlignmentCenter;
+    titleView.spacing = 6.0;
+
+    [NSLayoutConstraint activateConstraints:@[
+        [iconView.widthAnchor constraintEqualToConstant:19.0],
+        [iconView.heightAnchor constraintEqualToConstant:19.0]
+    ]];
+
+    self.navigationItem.titleView = titleView;
+}
+
 - (NSArray *)specifiers {
     if (!_specifiers) {
         _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
     return _specifiers;
+}
+
+- (void)openRepo {
+    NSURL *url = [NSURL URLWithString:@"https://github.com/551UK/PillVolume"];
+    if (!url) return;
+
+    UIApplication *application = UIApplication.sharedApplication;
+    if ([application respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+        [application openURL:url options:@{} completionHandler:nil];
+    } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        [application openURL:url];
+#pragma clang diagnostic pop
+    }
 }
 
 - (void)respring {
